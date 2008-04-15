a5a4e65628fc5ae7ddfc0024ee56c46906e3c02b
ceriel <ceriel@aaf88347-d911-0410-b711-e54d386773bb>
2008-04-15 16:11:04 +0000

    A few changes: - Global Result Table is now default again - removed some old comment

--- src/ibis/satin/impl/loadBalancing/Victim.java
+++ src/ibis/satin/impl/loadBalancing/Victim.java
@@ -184,10 +184,6 @@ public final class Victim implements Config {
     }
 
     public static String clusterOf(IbisIdentifier id) {
-        // Not correct: considers all nodes to be in different clusters
-        // if there is only one level. --Ceriel
-        // int count = id.location().numberOfLevels();
-        // return id.location().getLevel(count-1);
         return id.location().getParent().toString();
     }
 }
