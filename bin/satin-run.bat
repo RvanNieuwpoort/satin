251993fc04cc50ca6b293d8ad2f5dd9e4d61b673
ceriel <ceriel@aaf88347-d911-0410-b711-e54d386773bb>
2008-04-21 08:38:22 +0000

    Added

---
+++ bin/satin-run.bat
@@ -0,0 +1,24 @@
+@echo off
+
+if "%OS%"=="Windows_NT" @setlocal
+
+rem %~dp0 is expanded pathname of the current script under NT
+
+if "%SATIN_HOME%X"=="X" set SATIN_HOME=%~dp0..
+
+set JAVACLASSPATH=%CLASSPATH%;
+for %%i in ("%SATIN_HOME%\lib\*.jar") do call "%SATIN_HOME%\bin\AddToClassPath.bat" %%i
+
+set SATIN_APP_ARGS=
+
+:setupArgs
+if ""%1""=="""" goto doneStart
+set SATIN_APP_ARGS=%SATIN_APP_ARGS% "%1"
+shift
+goto setupArgs
+
+:doneStart
+
+java -classpath "%JAVACLASSPATH%" -Dlog4j.configuration=file:"%SATIN_HOME%"\log4j.properties %SATIN_APP_ARGS%
+
+if "%OS%"=="Windows_NT" @endlocal
