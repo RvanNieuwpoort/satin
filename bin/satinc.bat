6c0a7120f43a94cb624d28722b137a65d9c13b06
ceriel <ceriel@aaf88347-d911-0410-b711-e54d386773bb>
2008-06-03 07:48:48 +0000

    Oops: forgot to test satinc.bat; It did not work!

--- bin/satinc.bat
+++ bin/satinc.bat
@@ -10,6 +10,8 @@ set JAVACLASSPATH=%CLASSPATH%;
 for %%i in ("%SATIN_HOME%\lib\*.jar") do call "%SATIN_HOME%\bin\AddToSatinClassPath.bat" %%i
 
 set IBISC_ARGS=
+
+:setupArgs
 if ""%1""=="""" goto doneStart
 set IBISC_ARGS=%IBISC_ARGS% "%1"
 shift
